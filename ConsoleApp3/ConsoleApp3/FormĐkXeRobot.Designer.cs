2d6ab5511cabebea7b8d8632985a21b1717627f2
qda123 <98136812+qda123@users.noreply.github.com>
2024-01-11 20:54:58 +0700

    Add files via upload

--- ConsoleApp3/ConsoleApp3/FormĐkXeRobot.Designer.cs
+++ ConsoleApp3/ConsoleApp3/FormĐkXeRobot.Designer.cs
@@ -39,60 +39,170 @@ namespace ConsoleApp3
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Location = new System.Drawing.Point(0, 0);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.Location = new System.Drawing.Point(0, 0);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(525, 39);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "Xe tránh vật cản kết hợp dò line";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(307, 22);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(100, 29);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Chế độ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Location = new System.Drawing.Point(50, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 25);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Location = new System.Drawing.Point(200, 50);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 25);
+            this.comboBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(59, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 61);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Điều khiển bluetooth";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(59, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Chế độ tránh vật cản";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Blue;
+            this.button3.Location = new System.Drawing.Point(59, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 56);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Chế độ dò line";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.BlueViolet;
+            this.button4.Location = new System.Drawing.Point(537, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 77);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Tiến lên";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.BlueViolet;
+            this.button5.Location = new System.Drawing.Point(408, 241);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 76);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Rẽ trái";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.BlueViolet;
+            this.button6.Location = new System.Drawing.Point(537, 241);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(83, 76);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Lùi xuống";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.BlueViolet;
+            this.button7.Location = new System.Drawing.Point(668, 241);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 76);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Rẽ phải";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // FormĐkXeRobot
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormĐkXeRobot";
             this.Text = "FormĐkXeRobot";
-            this.Load += new System.EventHandler(this.FormĐkXeRobot_Load_1);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
     }
 }
--- ConsoleApp3/ConsoleApp3/FormĐkXeRobot.cs
+++ ConsoleApp3/ConsoleApp3/FormĐkXeRobot.cs
@@ -17,24 +17,67 @@ namespace ConsoleApp3
         public FormĐkXeRobot()
         {
             InitializeComponent();
-            string[] Baudrate = { "1200", "2400", "4800", "9600", "115200" };
-            comboBox2.Items.AddRange(Baudrate);
-            Control.CheckForIllegalCrossThreadCalls = false;
-        }
-        int l0 = 0, l2 = 0, l4 = 0, l5 = 0, l7 = 0, l9 = 0, l10 = 0;
-
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
 
-        private void btnGoAhead_Click(object sender, EventArgs e)
+        int l0 = 0, l1 = 0, l2 = 0, l3 = 0, l4 = 0, l5 = 0, l6 = 0;
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            if (serialPort1.IsOpen)
-            {
-                if (l2 == 0)
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+                if (l6 == 0)
+                {
+                    byte[] chr = new byte[] { 24 };
+                    serialPort1.Write(chr, 0, 1);
+                    l6++;
+                }
+                else
+                {
+
+                    serialPort1.Write("0");
+                    l6--;
+                }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+                if (l0 == 0)
                 {
                     serialPort1.Write("2");
+                    l0++;
+                }
+                else
+                {
+                    serialPort1.Write("0");
+                    l0--;
+                }   
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+                if (l1 == 0)
+                {
+                    serialPort1.Write("4");
+                    l1++;
+                }
+                else
+                {
+                    serialPort1.Write("0");
+                    l1--;
+                }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+                if (l2 == 0)
+                {
+                    serialPort1.Write("7");
                     l2++;
                 }
                 else
@@ -42,404 +85,20 @@ namespace ConsoleApp3
                     serialPort1.Write("0");
                     l2--;
                 }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
         }
 
-        private void btnGoRight_Click(object sender, EventArgs e)
+        private void button7_Click(object sender, EventArgs e)
         {
-            if (serialPort1.IsOpen)
-            {
-                if (l5 == 0)
+                if (l3 == 0)
                 {
                     serialPort1.Write("5");
-                    l5++;
+                    l3++;
                 }
                 else
                 {
                     serialPort1.Write("0");
-                    l5--;
+                    l3--;
                 }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-
-        private void btnGoLeft_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l4 == 0)
-                {
-                    serialPort1.Write("4");
-                    l4++;
-                }
-                else
-                {
-                    serialPort1.Write("0");
-                    l4--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnGoBack_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l7 == 0)
-                {
-                    serialPort1.Write("7");
-                    l7++;
-                }
-                else
-                {
-                    serialPort1.Write("0");
-                    l7--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnXoayPhai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l10 == 0)
-                {
-                    byte[] chr = new byte[] { 10 };
-                    serialPort1.Write(chr, 0, 1);
-                    l10++;
-                }
-                else
-                {
-
-                    serialPort1.Write("0");
-                    l10--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnXoayTrai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l9 == 0)
-                {
-                    serialPort1.Write("9");
-                    l9++;
-                }
-                else
-                {
-                    serialPort1.Write("0");
-                    l9--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnLine_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l24 == 0)
-                {
-                    byte[] chr = new byte[] { 24 };
-                    serialPort1.Write(chr, 0, 1);
-                    l24++;
-                }
-                else
-                {
-
-                    serialPort1.Write("0");
-                    l24--;
-                }
-
-
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnUltra_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l26 == 0)
-                {
-                    byte[] chr = new byte[] { 26 };
-                    serialPort1.Write(chr, 0, 1);
-                    l26++;
-                }
-                else
-                {
-                    serialPort1.Write("0");
-                    l26--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-            luu++;
-            lblSave.Text = luu.ToString();
-        }
-
-        private void btnnReset_Click(object sender, EventArgs e)
-        {
-            lblSave.Text = "0";
-        }
-
-        private void btnS1Trai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l16 == 0)
-                {
-                    byte[] chr = new byte[] { 16 };
-                    serialPort1.Write(chr, 0, 1);
-                    l16++;
-                }
-                else
-                {
-                    byte[] chr = new byte[] { 0 };
-                    serialPort1.Write(chr, 0, 1);
-                    l16--;
-                }
-
-
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnS1Phai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l17 == 0)
-                {
-                    byte[] chr = new byte[] { 17 };
-                    serialPort1.Write(chr, 0, 1);
-                    l17++;
-                }
-                else
-                {
-                    byte[] chr = new byte[] { 0 };
-                    serialPort1.Write(chr, 0, 1);
-                    l17--;
-                }
-
-
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnS2Trai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l18 == 0)
-                {
-                    byte[] chr = new byte[] { 18 };
-                    serialPort1.Write(chr, 0, 1);
-                    l18++;
-                }
-                else
-                {
-                    byte[] chr = new byte[] { 0 };
-                    serialPort1.Write(chr, 0, 1);
-                    l18--;
-                }
-
-
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnS2Phai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l19 == 0)
-                {
-                    byte[] chr = new byte[] { 19 };
-                    serialPort1.Write(chr, 0, 1);
-                    l19++;
-                }
-                else
-                {
-                    byte[] chr = new byte[] { 0 };
-                    serialPort1.Write(chr, 0, 1);
-                    l19--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnS3Trai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l20 == 0)
-                {
-                    byte[] chr = new byte[] { 20 };
-                    serialPort1.Write(chr, 0, 1);
-                    l20++;
-                }
-                else
-                {
-                    byte[] chr = new byte[] { 0 };
-                    serialPort1.Write(chr, 0, 1);
-                    l20--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnS3Phai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l21 == 0)
-                {
-                    byte[] chr = new byte[] { 21 };
-                    serialPort1.Write(chr, 0, 1);
-                    l21++;
-                }
-                else
-                {
-                    byte[] chr = new byte[] { 0 };
-                    serialPort1.Write(chr, 0, 1);
-                    l21--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnS4Trai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l22 == 0)
-                {
-                    byte[] chr = new byte[] { 22 };
-                    serialPort1.Write(chr, 0, 1);
-                    l22++;
-                }
-                else
-                {
-                    byte[] chr = new byte[] { 0 };
-                    serialPort1.Write(chr, 0, 1);
-                    l22--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnS4Phai_Click(object sender, EventArgs e)
-        {
-            if (serialPort1.IsOpen)
-            {
-                if (l23 == 0)
-                {
-                    byte[] chr = new byte[] { 23 };
-                    serialPort1.Write(chr, 0, 1);
-                    l23++;
-                }
-                else
-                {
-                    byte[] chr = new byte[] { 0 };
-                    serialPort1.Write(chr, 0, 1);
-                    l23--;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn chưa kết nối cổng Port");
-            }
-        }
-
-        private void btnConnect_Click(object sender, EventArgs e)
-        {
-            if (!serialPort1.IsOpen)
-            {
-                btnConnect.Text = "DISCONNECT";
-                serialPort1.PortName = comboBox1.Text;
-                serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
-                serialPort1.Open();
-                lblKetNoi.ForeColor = Color.Blue;
-                btnConnect.BackColor = Color.Blue;
-                lblKetNoi.Text = "Đã kết nối";
-            }
-            else
-            {
-                btnConnect.Text = "CONNECT";
-                serialPort1.Close();
-                lblKetNoi.ForeColor = Color.Red;
-                btnConnect.BackColor = Color.Red;
-                lblKetNoi.Text = "Chưa kết nối";
-            }
-        }
-
-        int l16 = 0, l17 = 0, l18 = 0, l19 = 0, l20 = 0, l21 = 0, l22 = 0, l23 = 0;
-        int l24 = 0, l25 = 0, l26 = 0, l27 = 0;
-        int luu = 0;
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void FormĐkXeRobot_Load_1(object sender, EventArgs e)
-        {
-            comboBox1.DataSource = SerialPort.GetPortNames();
-            comboBox2.Text = "9600";
         }
     }
 }
